fb7b41b2f0f45d53bce91f4bdbe9364d977e4dc6
PinkCar <102372720+PinkCar520@users.noreply.github.com>
2026-04-28 10:25:48 +0800

    fix

---
+++ apps/mobile/harmony/entry/hvigorfile.ts
@@ -0,0 +1,6 @@
+import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+
+export default {
+    system: hapTasks,  /* Built-in tasks of Hvigor */
+    plugins:[]         /* Custom plugins to extend the functionality of Hvigor */
+}
---
+++ apps/mobile/harmony/hvigorfile.ts
@@ -0,0 +1,6 @@
+import { appTasks } from '@ohos/hvigor-ohos-plugin';
+
+export default {
+    system: appTasks,  /* Built-in tasks of Hvigor */
+    plugins:[]         /* Custom plugins to extend the functionality of Hvigor */
+}
